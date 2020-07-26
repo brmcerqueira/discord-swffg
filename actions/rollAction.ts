d82d2c0df9c1a2490889bb9da5b9e38d5cf5cfdb
Bruno <brmcerqueira@gmail.com>
2020-07-26 17:27:57 -0300

    Ajuste

--- actions/rollAction.ts
+++ actions/rollAction.ts
@@ -3,7 +3,7 @@ import { Message } from "katana/mod.ts";
 export function rollAction(message: Message, matchArray: RegExpMatchArray[]) {
     for(let match of matchArray) {
         if (match.groups) {
-   
+            console.log(match.groups);
         }      
     }
 }
--- main.ts
+++ main.ts
@@ -15,7 +15,7 @@ type RegExpAction = {
 }
 
 const regExpActions: RegExpAction[] = [{
-  regex: /^%(?<dices>[1-9]?\d)\s*(\!(?<hunger>[1-5]))?\s*(\*(?<difficulty>[2-9]))?\s*(?<description>.*)/g,
+  regex: /^%\s*(\+(?<skill>[1-9]?\d))?\s*(\*\+(?<proficiency>[1-9]?\d))?\s*(\+\+(?<enlargement>[1-9]?\d))?\s*(\-(?<difficulty>[1-9]?\d))?\s*(\*\-(?<challenge>[1-9]?\d))?\s*(\-\-(?<setback>[1-9]?\d))?\s*(?<description>.*)/g,
   action: rollAction
 }];
 
